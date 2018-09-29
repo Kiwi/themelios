588b3409416cd08748599865ccbe7e73e979e9e5
Adam Schaefers <sch@efers.org>
2018-09-28 20:06:28 -0700

    test

--- hosts/vm-example/configuration.sh
+++ hosts/vm-example/configuration.sh
@@ -13,7 +13,7 @@ zfs_pool_type=""           # use "" for single, or "mirror", "raidz1", etc.
 
 # Note: using /dev/disk/by-id is also preferable.
 #zfs_pool_disks=("/dev/sda" "/dev/sdb")
-zfs_pool_disks=("/dev/disk/by-id/ata-QEMU_HARDDISK_QM00003")
+zfs_pool_disks=("/dev/sda")
 
 # # Datasets to be set with com.sun:auto-snapshot=true.
 zfs_auto_snapshot=("$zfs_pool_name/HOME" "$zfs_pool_name/ROOT")
